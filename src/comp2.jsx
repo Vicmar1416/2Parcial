c329991b7cce61bf1c447ff82abce39015477e76
Victor Alvarez <bolivarisimo1404@gmail.com>
2024-05-10 13:37:44 -0400

    primer commit

---
+++ src/comp2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import P2Img from './P2Img.jpg';
+
+function Comp2(){
+    return(
+        <div>
+            <img src="{P2Img}" alt='P2Img'/>
+        </div>
+    );
+}
+
+export default Comp2;
